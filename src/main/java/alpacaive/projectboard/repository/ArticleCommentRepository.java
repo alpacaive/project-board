7df4813b392543e3c13c519d44805dc92ee7efcc
alpacaive <w6352800@naver.com>
2025-02-11 21:19:15 +0900

    #2 - data rest 설정
    게시글, 댓글의 json api 를 자동으로 restful 하게 만들게끔 설정

--- src/main/java/alpacaive/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/alpacaive/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,9 @@ package alpacaive.projectboard.repository;
 
 import alpacaive.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
