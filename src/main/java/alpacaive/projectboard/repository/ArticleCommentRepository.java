2169e2cfae67e6bcc2ba2484ac8a67101ae67fab
alpacaive <w6352800@naver.com>
2025-02-14 04:07:18 +0900

    #2 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/alpacaive/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/alpacaive/projectboard/repository/ArticleCommentRepository.java
@@ -1,10 +1,28 @@
 package alpacaive.projectboard.repository;
 
 import alpacaive.projectboard.domain.ArticleComment;
+import alpacaive.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);  // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
