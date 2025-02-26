1fbdc5f0c1a52511cc2b5250ec2778540f229297
alpacaive <w6352800@naver.com>
2025-02-26 22:27:51 +0900

    #19 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    시간 상 기능의 일부만 테스트로 표현
    나머지 필요 기능은 별도로 작성하여 넣기로 함.

---
+++ src/main/java/alpacaive/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,20 @@
+package alpacaive.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link alpacaive.projectboard.domain.ArticleComment}
+ */
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content
+) {
+
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+
+}
