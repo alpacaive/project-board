e6472777aeae3e607f29253f78057e120f49b615
alpacaive <w6352800@naver.com>
2025-02-26 22:26:04 +0900

    #19 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto와 enum을 함께 작성했는데, 추후에 수정할 수도 있음

---
+++ src/main/java/alpacaive/projectboard/dto/ArticleDto.java
@@ -0,0 +1,20 @@
+package alpacaive.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link alpacaive.projectboard.domain.Article}
+ */
+public record ArticleDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
+        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+    }
+
+}
---
+++ src/main/java/alpacaive/projectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,16 @@
+package alpacaive.projectboard.dto;
+
+/**
+ * DTO for {@link alpacaive.projectboard.domain.Article}
+ */
+public record ArticleUpdateDto(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleUpdateDto of(String title, String content, String hashtag) {
+        return new ArticleUpdateDto(title, content, hashtag);
+    }
+
+}
