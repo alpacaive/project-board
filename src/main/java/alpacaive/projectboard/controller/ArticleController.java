b2ae297d9b0bbce4d325f05927f1d26bf89be62f
alpacaive <w6352800@naver.com>
2025-03-06 06:42:57 +0900

    #36 - 리팩토링: UserAccountDto 에 메타 정보를 넣지 않는 of() 팩토리 메소드 추가
    테스트를 용이하게 하기 위해 추가함
    
    테스트 목적 외에도, repository 레이어에 영속되지 않는 객체는 메타 정보가 없는 것이 당연하므로 새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는 팩토리 메소드를 만들어주는 의미가 있음

--- src/main/java/alpacaive/projectboard/controller/ArticleController.java
+++ src/main/java/alpacaive/projectboard/controller/ArticleController.java
@@ -1,7 +1,9 @@
 package alpacaive.projectboard.controller;
 
+import alpacaive.projectboard.domain.constant.FormStatus;
 import alpacaive.projectboard.domain.constant.SearchType;
 import alpacaive.projectboard.dto.UserAccountDto;
+import alpacaive.projectboard.dto.request.ArticleRequest;
 import alpacaive.projectboard.dto.response.ArticleResponse;
 import alpacaive.projectboard.dto.response.ArticleWithCommentsResponse;
 import alpacaive.projectboard.service.ArticleService;
@@ -90,7 +92,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles";
@@ -110,7 +112,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
